78bbd215e77bd738aa8e1f95bb5fd3f7ed88b513
anubhavgupta1 <41202057+anubhavgupta1@users.noreply.github.com>
2021-07-23 22:20:43 +0530

    Add files via upload

---
+++ Unbounded KnapSack Problems/3. Coin Change 1/DP/Count Subset Sum Problem.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solve(int *arr, int sum, int n, vector<vector<int>> &dp)
+{
+    for(int i = 0; i< sum+1; i++)
+    {
+        dp[0][i] = 0;
+    }
+
+    for(int i = 0; i< n+1; i++)
+    {
+        dp[i][0] = 1;
+    }
+
+    for(int i = 1; i< n+1; i++)
+    {
+        for(int s = 1; s< sum+1; s++)
+        {
+            if(arr[i-1]<=s)
+            {
+                int a = dp[i][s-arr[i-1]];
+                int b = dp[i-1][s];
+                dp[i][s] = a + b;
+            }
+            else
+            {
+                dp[i][s] = dp[i-1][s];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+
+int solve(int *arr, int sum, int n)
+{
+    vector< vector<int>> dp(n+1, vector<int> (sum+1));
+    return solve(arr, sum, n, dp);
+}
+
+
+int main()
+{
+    int coins[] = {1, 2, 3};
+    int n = 3;
+    int sum = 5;
+    cout<<solve(coins, sum,n);
+}
